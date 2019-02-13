4ec1d3511c28f19fc58d4234395215a40b1c2750
Alireza Josheghani <josheghani.dev@gmail.com>
2019-02-13 21:41:54 +0330

    Add form validators in application

---
+++ app/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import *
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy
+
+
+def validate_exists(value):
+    user = User.objects.filter(username=value)
+
+    if not user.exists():
+        raise ValidationError(
+            gettext_lazy('User %(username)s does not exists!'),
+            params={'username': value},
+        )
+
+
+class CreateAuthTokenForm(forms.Form):
+    username = forms.CharField(max_length=100, required=True, validators=[validate_exists])
+    password = forms.CharField(max_length=100, required=True)
