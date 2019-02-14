087b482ac89b2d5f0f6d6f4086269e1cb08a4d9e
Alireza Josheghani <josheghani.dev@gmail.com>
2019-02-14 11:02:55 +0330

    Commented sentry initialize

--- app/settings.py
+++ app/settings.py
@@ -2,10 +2,10 @@ import os
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
-sentry_sdk.init(
-    dsn=os.getenv("SENTRY_DSN"),
-    integrations=[DjangoIntegration()]
-)
+# sentry_sdk.init(
+#     dsn=os.getenv("SENTRY_DSN"),
+#     integrations=[DjangoIntegration()]
+# )
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
