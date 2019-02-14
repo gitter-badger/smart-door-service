824f813d8946f5a0bc0b868e2d275324050d3907
Alireza Josheghani <josheghani.dev@gmail.com>
2019-02-14 11:03:20 +0330

    Added models relationships

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from .helpers import *
 
 
 class User(models.Model):
+    id = models.AutoField(primary_key=True)
     fullname = models.CharField(max_length=80)
     username = models.CharField(max_length=30)
     email = models.CharField(max_length=30)
@@ -22,7 +23,21 @@ class User(models.Model):
         db_table = 'users'
 
 
+class Device(models.Model):
+    id = models.AutoField(primary_key=True)
+    type = models.CharField(max_length=80)
+    os = models.CharField(max_length=80)
+    model = models.CharField(max_length=80)
+    icon = models.CharField(max_length=80)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'devices'
+
+
 class LogType(models.Model):
+    id = models.AutoField(primary_key=True)
     priority = models.CharField(max_length=80)
     pin_id = models.CharField(max_length=80)
     icon = models.CharField(max_length=80)
@@ -34,23 +49,13 @@ class LogType(models.Model):
 
 
 class Log(models.Model):
-    user_id = models.CharField(max_length=80)
-    type_id = models.CharField(max_length=80)
-    device_id = models.CharField(max_length=80)
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    device = models.ForeignKey(Device, on_delete=None, related_name='device')
+    type = models.ForeignKey(LogType, on_delete=None, related_name='type')
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    readed = models.BooleanField(default=False)
+    # updted_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = 'logs'
-
-
-class Device(models.Model):
-    type = models.CharField(max_length=80)
-    os = models.CharField(max_length=80)
-    model = models.CharField(max_length=80)
-    icon = models.CharField(max_length=80)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        db_table = 'devices'
