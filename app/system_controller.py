2821737ca3ca6582735134efec1810ac7481960c
Alireza Josheghani <josheghani.dev@gmail.com>
2019-02-23 07:46:05 +0330

    fixing a bug in adding api_base_uri as array instend of string

--- app/system_controller.py
+++ app/system_controller.py
@@ -12,22 +12,23 @@ def check(request):
     if os.getenv("BASE_URI") is not None and os.getenv("BASE_URI") is not "":
         uri = os.getenv("BASE_URI")
 
-    api_base_uri = uri + "/" + os.getenv("APP_VERSION"),
+    api_base_uri = uri + "/" + os.getenv("APP_VERSION")
+
     avatars_base_uri = uri + "/uploads/avatars"
 
     if os.getenv("API_BASE_URI") is not None and os.getenv("API_BASE_URI") is not "":
-        uri = os.getenv("API_BASE_URI")
+        api_base_uri = os.getenv("API_BASE_URI")
 
     if os.getenv("AVATARS_URI") is not None and os.getenv("AVATARS_URI") is not "":
         avatars_base_uri = os.getenv("AVATARS_URI")
 
     return respond.succeed({
-        "version": os.getenv("APP_VERSION"),
-        "base_uri": uri,
-        "api_base_uri": api_base_uri,
-        "avatars_base_uri": avatars_base_uri,
+        "version": str(os.getenv("APP_VERSION")),
+        "base_uri": str(uri),
+        "api_base_uri": str(api_base_uri),
+        "avatars_base_uri": str(avatars_base_uri),
         "system": {
-            "name": platform.system(),
-            "machine": machine
+            "name": str(platform.system()),
+            "machine": str(machine)
         }
     })
