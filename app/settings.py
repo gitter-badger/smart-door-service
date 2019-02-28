53e88fa1be35d8823fc722f98432d10298304703
Alireza Josheghani <josheghani.dev@gmail.com>
2019-03-01 00:25:35 +0330

    Removing sentry from project

--- app/settings.py
+++ app/settings.py
@@ -1,14 +1,6 @@
 import os
 from app import adapters
 
-# import sentry_sdk
-# from sentry_sdk.integrations.django import DjangoIntegration
-
-# sentry_sdk.init(
-#     dsn=os.getenv("SENTRY_DSN"),
-#     integrations=[DjangoIntegration()]
-# )
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
